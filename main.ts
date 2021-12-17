eb1933f99017526d9782018069502ad5572ce157
Martin1o1o2009 <94906587+Martin1o1o2009@users.noreply.github.com>
2021-12-17 12:48:01 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,33 @@
+input.onButtonPressed(Button.A, function () {
+    if (contador == 5) {
+        bloqueo = 0
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (control.millis() - tiempo < 1300) {
+        contador += 1
+    } else {
+        basic.showIcon(IconNames.Sad)
+        while (true) {
+            basic.pause(100)
+        }
+    }
+})
+let bloqueo = 0
+let tiempo = 0
+let contador = 0
+basic.showString("clave")
+contador = 0
+tiempo = control.millis()
+bloqueo = 1
+basic.forever(function () {
+    if (bloqueo == 0) {
+        basic.showString("Hello!")
+    }
+    if (tiempo - control.millis() > 1300) {
+        basic.showIcon(IconNames.Heart)
+        while (true) {
+            basic.pause(100)
+        }
+    }
+})
